8c038047183b2547b92ea81be2e51678b52ec367
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-05-23 19:46:46 +0545

    backend

--- backend/app.js
+++ backend/app.js
@@ -83,19 +83,35 @@ function handleLogin() {
 }
 
 function handleBookTicket() {
+  //   let sql = `ALTER TABLE bookings
+  // ADD COLUMN busid TEXT`;
+  // db.run(sql);
   app.post("/book", (req, res) => {
     try {
-      const { userid, name, source, destination, date, seat } = req.body;
+      const { userid, name, source, destination, date, seat, busid } = req.body;
       let sql =
-        "INSERT INTO bookings (userid,name,source,destination, date,seat) VALUES (?,?,?,?,?,?)";
-      db.run(sql, [userid, name, source, destination, date, seat], (e) => {
-        if (e != undefined) {
-          res.json({ success: false, message: e });
+        "INSERT INTO bookings (userid,name,source,destination, date,seat,busid) VALUES (?,?,?,?,?,?,?)";
+      db.run(
+        sql,
+        [userid, name, source, destination, date, seat, busid],
+        (e) => {
+          if (e != undefined) {
+            res.json({ success: false, message: e });
 
-          return console.log(e);
-        } else
-          res.json({ success: true, userid, name, source, destination, seat });
-      });
+            return console.log(e);
+          } else {
+            res.json({
+              success: true,
+              userid,
+              name,
+              source,
+              destination,
+              seat,
+              busid,
+            });
+          }
+        }
+      );
     } catch (e) {
       if (e) throw new Error(e);
       res.json({ success: false, message: "Some Error occured" });
@@ -132,10 +148,111 @@ function getUserDataFromUserId(uid, res) {
     }
   });
 }
+function addBus() {
+  // let sql1 =
+  //   "CREATE TABLE buses (busid INTEGER PRIMARY KEY,busName TEXT, source TEXT, destination TEXT , time TEXT , date TEXT, price INTEGER)";
+  // db.run("DROP TABLE buses", (e) => {
+  //   console.log("table dropped again");
+  // });
+  // db.run(
+  //   "CREATE TABLE buses (busid TEXT PRIMARY KEY,busName TEXT, source TEXT, destination TEXT , time TEXT , date TEXT,reservedSeats TEXT, price INTEGER)",
+  //   (e) => {
+  //     if (e) console.log(e.message);
+  //     console.log("table MNODI");
+  //   }
+  // );
+  let sql = app.post("/addBus", (req, res) => {
+    try {
+      const {
+        busid = "9888",
+        busName = "Agni Travels",
+        source = "kathmandu",
+        destination = "pokhara",
+        date = "2023 05 25",
+        time = "18 00",
+        reservedSeats = "22 11 4",
+        price = 1000,
+      } = req.body;
 
+      let sql =
+        "INSERT INTO  buses (busid , busName, source , destination  , date  , time ,reservedSeats, price ) VALUES (?,?,?,?,?,?,?,?)";
+
+      db.run(
+        sql,
+        [busid, busName, source, destination, date, time, reservedSeats, price],
+        (e) => {
+          if (e) {
+            console.log(e);
+            res.json({
+              success: false,
+              message: "the following data has not been inserted",
+              error: e.message,
+            });
+          } else {
+            console.log("Values Inserted");
+            res.json({
+              success: true,
+              message: "the following data has been inserted",
+            });
+          }
+        }
+      );
+    } catch (e) {
+      res.json({
+        status: "failure",
+        message: e.message,
+      });
+      return console.error(e);
+    }
+  });
+}
+function searchBus() {
+  app.get("/search", (req, res) => {
+    const { source, destination, date } = req.body;
+    let sql = "SELECT * FROM buses WHERE source=? AND destination=? AND date=?";
+    db.all(sql, [source, destination, date], (err, rows) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: "UNABLE TO FIND BUSES",
+          err: err.message,
+        });
+      } else {
+        let busList = [];
+        rows.forEach((row) => {
+          busList.push(row);
+        });
+        res.json({ success: true, buses: busList });
+      }
+    });
+  });
+}
+function getAllBuses() {
+  app.get("/getBuses", (req, res) => {
+    let sql = "SELECT * FROM buses";
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: "UNABLE TO FIND BUSES",
+          err: err.message,
+        });
+      } else {
+        let busList = [];
+        rows.forEach((row) => {
+          busList.push(row);
+        });
+        res.json({ success: true, buses: busList });
+      }
+    });
+  });
+}
 //todo
+getAllBuses();
+searchBus();
 handleLogin();
 handleBookTicket();
-
 handleSignup();
+addBus();
+
 app.listen(3000);
