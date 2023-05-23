29d6e13f22191c690b19164ff7ed32a7a45d6dfa
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-05-23 10:24:48 +0545

    login and signup backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const sqlite3 = require("sqlite3").verbose();
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+
 const db = new sqlite3.Database("test.db", (err) => {
   if (err) {
     return console.error(err.message);
@@ -11,21 +13,18 @@ const db = new sqlite3.Database("test.db", (err) => {
 const app = express();
 app.use(bodyParser.json());
 
-let command =
-  "CREATE TABLE contacts (contact_id INTEGER PRIMARY KEY,first_name TEXT NOT NULL,last_name TEXT NOT NULL,email TEXT NOT NULL UNIQUE,phone TEXT NOT NULL UNIQUE)";
-let c = "DROP TABLE users";
-// db.run(sql);
-
 function handleSignup() {
   // let sql =
   //   "CREATE TABLE users (userid INTEGER PRIMARY KEY,Name TEXT, Email TEXT UNIQUE, Password TEXT NOT NULL)";
   app.post("/signup", (req, res) => {
     try {
       const { id, name, email, password } = req.body;
+      var salt = bcrypt.genSaltSync(10);
+      var hashedPassword = bcrypt.hashSync(password, salt);
       let sql =
         "INSERT INTO users(userid,Name, Email, Password) VALUES (?,?,?,?)";
 
-      db.run(sql, [id, name, email, password], (err) => {
+      db.run(sql, [id, name, email, hashedPassword], (err) => {
         if (err) {
           res.json({
             status: "failure",
@@ -49,8 +48,39 @@ function handleSignup() {
     }
   });
 }
-db.run("DROP table contacts");
+function handleLogin() {
+  app.post("/login", (req, res) => {
+    try {
+      console.warn("login request :post");
+      const { email, password } = req.body;
+      console.warn(email, password);
+
+      let sql = `SELECT password,userid,Name,Email FROM users WHERE email=?`;
+      db.all(sql, [email], (err, row) => {
+        if (row[0]) {
+          let { Password: hash, userid, Name, Email } = row[0];
+          console.log(hash, userid, Name, Email);
+          let isAuthorized = bcrypt.compareSync(password, hash);
+          console.log(isAuthorized);
+          if (isAuthorized) {
+            res.json({ authorized: true, Name, Email, userid });
+          } else {
+            res.json({ authorized: false });
+          }
+        } else {
+          res.json({ authorized: false, message: "No Such User" });
+        }
+      });
+    } catch (e) {
+      res.json({
+        status: "failure",
+        message: e.message,
+      });
+      return console.error(e);
+    }
+  });
+}
+handleLogin();
+
 handleSignup();
 app.listen(3001);
-
-db.close((e) => console.log(e));
