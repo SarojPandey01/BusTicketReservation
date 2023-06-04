e0f05a8764d53131f0080386cf0f4d108fe7ff1a
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-06-04 14:13:26 +0545

    forgot password

--- seatLayout.js
+++ seatLayout.js
@@ -55,12 +55,7 @@ function handleClick(e) {
 
     authorized
       ? handleBookReservation(reservedSeats, userdata)
-      : (document.body.innerHTML = `<div class="block min-w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-200 w-screen grid min-h-screen place-items-center" > <dialog class="p-12 w-56 h-60 backdrop:bg-gray-400 rounded backdrop:bg-opacity-50 flex justify-center flex-col align-center open:animate-fade-in open:backdrop:animate-fade-in" open>
-      <p class="mb-6 text-lg font-bold text-black">Login First to continue</p>
-      <form>
-        <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" >OK</button>
-      </form>
-    </dialog></div>`);
+      : alert("Please login");
   };
 }
 let modalContainer = document.getElementById("modal-container");
