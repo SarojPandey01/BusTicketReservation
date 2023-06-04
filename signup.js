0f21bd8aa53633bc002f03c9a3a7cab681d236bf
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-06-04 15:48:14 +0545

    finalize

--- signup.js
+++ signup.js
@@ -21,6 +21,19 @@ form.addEventListener("submit", async (e) => {
   let password2 = pass2Input.value;
   let isPasswordValid = false;
   let isPhoneValid = false;
+  let hasPw = false;
+  if (password1.length <= 6) {
+    let dialog = `<div class="block min-w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-200 w-screen grid min-h-screen place-items-center" > <dialog class="p-12 w-56 h-60 backdrop:bg-gray-400 rounded backdrop:bg-opacity-50 flex justify-center flex-col align-center open:animate-fade-in open:backdrop:animate-fade-in" open>
+      <p class="mb-6 text-lg font-bold text-black">Password must contain at least 6 characters. Please try again</p>
+      <form>
+        <button class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" >OK</button>
+      </form>
+    </dialog></div>`;
+    document.body.innerHTML = dialog;
+    // hasPw=false
+  } else {
+    hasPw = true;
+  }
   if (password1 != password2) {
     let dialog = `<div class="block min-w-1/2 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-200 w-screen grid min-h-screen place-items-center" > <dialog class="p-12 w-56 h-60 backdrop:bg-gray-400 rounded backdrop:bg-opacity-50 flex justify-center flex-col align-center open:animate-fade-in open:backdrop:animate-fade-in" open>
       <p class="mb-6 text-lg font-bold text-black">Passwords Do Not Match</p>
@@ -46,7 +59,7 @@ form.addEventListener("submit", async (e) => {
   }
   let id = Math.floor(100000 + Math.random() * 900000);
 
-  if (isPasswordValid && isPhoneValid) {
+  if (isPasswordValid && isPhoneValid && hasPw) {
     const data = {
       id,
       name,
