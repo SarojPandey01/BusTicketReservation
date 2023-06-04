fa3f9128b46be4da9a1a6d4f0fb41e12b6ef8742
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-06-04 15:18:16 +0545

    forgot password

--- forPass.js
+++ forPass.js
@@ -3,20 +3,156 @@ let emailInput = document.getElementById("email");
 let phoneInput = document.getElementById("phoneno");
 let password00Input = document.getElementById("password");
 let password01Input = document.getElementById("password1");
-let submitInput = document.getElementById("submit");
+let submitInput = document.getElementById("submitbtn");
+let formMain = document.getElementById("formMain");
 
+formMain.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  submitInput.innerHTML = `<svg aria-hidden="true" class="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>`;
+  submitInput.disabled = true;
 
-formInput.addEventListener("submit", async (e) => {
-    e.preventDefault();
-  
-    let email = emailInput.value;
-    let phoneno = phoneInput.value;
-    let password00 = passwordInput.value;
-    let password1 = password01Input.value;
-    let form = formInput.value;
-    console.log(name);
-    console.log(password);
-    const data = {
-      email: name,
-      password,
-    };
+  let email = emailInput.value;
+  let phoneno = phoneInput.value;
+  let password0 = password00Input.value;
+  let password1 = password01Input.value;
+
+  if (password0 === password1) {
+    let data = { email, phone: phoneno, newPassword: password0 };
+    console.log("sending reqiest");
+    // let res = await fetch(`${API_URL}/forgot`, {
+    //   method: "POST",
+    //   body: JSON.stringify(data),
+    //   headers: {
+    //     "content-type": "Application/json",
+    //   },
+
+    // });
+    let res = await fetch(`${API_URL}/forgot`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      cors: "no-cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(res);
+    let { success, message } = await res.json();
+    console.log({ success, message });
+    if (success) {
+      formInput.innerHTML = `        <div
+          id="alert-additional-content-3"
+          class="p-4  mb-4 text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800"
+          role="alert"
+        >
+          <div class="flex items-center">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 mr-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <h3 class="text-lg font-medium">CONGRATULATIONS</h3>
+          </div>
+          <div class="mt-2 mb-4 text-sm">
+            Just like a phoenix rising from the ashes, your old password has
+            gracefully retired, making way for a stronger and more resilient
+            guardian of your digital fortress. 🌅🔒
+          </div>
+          <div class="flex">
+            <a href="./login.html">
+              <button
+                type="button"
+                class="text-white bg-green-800 hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-log-in"
+                >
+                  <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+                  <polyline points="10 17 15 12 10 7"></polyline>
+                  <line x1="15" y1="12" x2="3" y2="12"></line>
+                </svg>
+                &nbsp; Go to Login Page
+              </button>
+            </a>
+          </div>
+
+        </div>`;
+    } else {
+      formInput.innerHTML = `     <div
+          id="alert-additional-content-2"
+          class="p-4  place-items-center mb-4 text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          role="alert"
+        >
+          <div class="flex items-center">
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 mr-2"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Info</span>
+            <h3 class="text-lg font-medium">Failed to change password</h3>
+          </div>
+          <div class="mt-2 mb-4 text-sm">
+            Our team has encountered a temporary roadblock while attempting to
+            implement the requested password change. We understand the
+            importance of securing your account and apologize for any
+            inconvenience this may cause. 🙇‍♂️🔒
+          </div>
+          <div class="flex">
+            <a href="./index.html">
+              <button
+                type="button"
+                class="text-red-800 bg-transparent border border-red-800 hover:bg-red-900 hover:text-white focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-3 py-1.5 text-center dark:hover:bg-red-600 dark:border-red-600 dark:text-red-500 dark:hover:text-white dark:focus:ring-red-800"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 50 50"
+                  width="20px"
+                  height="20px"
+                  fill="#fff"
+                >
+                  <path
+                    d="M 25 1.0507812 C 24.7825 1.0507812 24.565859 1.1197656 24.380859 1.2597656 L 1.3808594 19.210938 C 0.95085938 19.550938 0.8709375 20.179141 1.2109375 20.619141 C 1.5509375 21.049141 2.1791406 21.129062 2.6191406 20.789062 L 4 19.710938 L 4 46 C 4 46.55 4.45 47 5 47 L 19 47 L 19 29 L 31 29 L 31 47 L 45 47 C 45.55 47 46 46.55 46 46 L 46 19.710938 L 47.380859 20.789062 C 47.570859 20.929063 47.78 21 48 21 C 48.3 21 48.589063 20.869141 48.789062 20.619141 C 49.129063 20.179141 49.049141 19.550938 48.619141 19.210938 L 25.619141 1.2597656 C 25.434141 1.1197656 25.2175 1.0507812 25 1.0507812 z M 35 5 L 35 6.0507812 L 41 10.730469 L 41 5 L 35 5 z"
+                  />
+                </svg>
+                Go to Home Page
+              </button>
+            </a>
+          </div>
+        </div>`;
+    }
+  } else {
+    alert("Two password do not match. Try again");
+  }
+  submitInput.innerHTML = `Submit`;
+  submitInput.disabled = false;
+  console.log({ email, phoneno, password0, password1 });
+});
